c203587e68747d00cc09a59baf090a50aa0e7fec
Arunabha <69160160+ag2810@users.noreply.github.com>
2022-04-22 23:25:31 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (83.19%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int fib(int n, int dp[])
+    {
+        if(n <= 1)
+        {
+            dp[n] = 1;
+            return dp[n];
+        }
+    
+        if(dp[n] != -1)
+            return dp[n];
+        
+        dp[n] = fib(n - 1, dp) + fib(n - 2, dp);
+        
+        return dp[n];
+    }
+    int climbStairs(int n) 
+    {
+        int dp[n + 1];
+        
+        memset(dp, -1, sizeof(dp));
+        
+        fib(n, dp);
+        
+        return dp[n];
+    }
+};
