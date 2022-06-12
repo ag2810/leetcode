474600e237448ff6778eb7e126a73607768d84d7
Arunabha <69160160+ag2810@users.noreply.github.com>
2022-06-12 22:26:15 +0530

    Time: 204 ms (30.50%), Space: 14.4 MB (22.51%) - LeetHub

--- 526-beautiful-arrangement/526-beautiful-arrangement.cpp
+++ 526-beautiful-arrangement/526-beautiful-arrangement.cpp
@@ -1,25 +1,28 @@
 class Solution {
 public:
     int ans;
-    void countArrangementUtil(int index, vector<int>& nums) {
+    void countArrangementUtil(int index, vector<vector<int>>& temp, vector<int>& nums) {
         if(index == nums.size()) {
             ans++;
+            temp.push_back(nums);
             return;
         }
         
         for(int i = index;i < nums.size();i++) {
-            swap(nums[index], nums[i]);
-            if((nums[index] % (index + 1) == 0) || ((index + 1) % nums[index] == 0)) 
-                countArrangementUtil(index + 1, nums);
-            swap(nums[index], nums[i]);
+            if((nums[i] % (index + 1) == 0) || ((index + 1) % nums[i] == 0)) {
+                swap(nums[index], nums[i]);
+                countArrangementUtil(index + 1, temp, nums);
+                swap(nums[index], nums[i]);
+            }
         }
     }
     
     int countArrangement(int n) {
         ans = 0;
         vector<int> nums;
+        vector<vector<int>> temp;
         for(int i = 0;i < n;i++) nums.push_back(i + 1);
-        countArrangementUtil(0, nums);
+        countArrangementUtil(0, temp, nums);
         return ans;
     }
 };
